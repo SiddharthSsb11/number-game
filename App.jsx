ed782e712da35a94b0c285bcc46022dc5994c1fd
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-03-21 23:31:37 +0530

    refactor: initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './app.styles';
+const App = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.boxOne}>React Native</Text>
+    </View>
+  );
+};
+
+export default App;
